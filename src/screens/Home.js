194a99508876e689d2d60f97807b23b096748130
Qulbe12 <qhussain@intelligentsoftware.net>
2022-06-29 08:00:47 -0700

    save

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,95 @@
-import { View, StatusBar, ScrollView, TouchableOpacity } from "react-native";
-import React from "react";
-import { COLOURS } from "../components/Database";
-import Entypo from "react-native-vector-icons";
+import {
+  View,
+  StatusBar,
+  ScrollView,
+  TouchableOpacity,
+  Text,
+  Image,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { COLOURS, Items } from "../components/Database";
+import Entypo from "react-native-vector-icons/Entypo";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-const Home = () => {
+const Home = ({ navigation }) => {
+  const [products, setproducts] = useState([]);
+  const [accessory, setaccessory] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      getDataFromDB();
+    });
+  }, [navigation]);
+
+  const getDataFromDB = () => {
+    let productList = [];
+    let accessoryList = [];
+
+    Items.map((item) => {
+      if (item.category == "product") {
+        productList.push(item);
+      } else if (item.category == "accessory") {
+        accessoryList.push(item);
+      }
+
+      setproducts(productList);
+      setaccessory(accessoryList);
+
+      console.log(products);
+    });
+  };
+
+  const ProductCard = ({ data }) => {
+    return (
+      <TouchableOpacity
+        style={{
+          width: "48%",
+          marginVertical: 14,
+        }}
+      >
+        <View
+          style={{
+            width: "100%",
+            height: 100,
+            borderRadius: 100,
+            backgroundColor: COLOURS.backgroundLight,
+            position: "relative",
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: 8,
+          }}
+        >
+          {data.isOff ? (
+            <View
+              style={{
+                position: "absolute",
+                width: "20%",
+                height: "24%",
+                backgroundColor: COLOURS.green,
+                top: 0,
+                left: 0,
+                borderTopLeftRadius: 10,
+                borderBottomRightRadius: 10,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text>{data.offPercentage}</Text>
+            </View>
+          ) : null}
+          <Image
+            source={data.productImage}
+            style={{
+              width: "80%",
+              height: "80%",
+              resizeMode: "contain",
+            }}
+          />
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View
       style={{
@@ -22,7 +108,107 @@ const Home = () => {
             padding: 16,
           }}
         >
-          <TouchableOpacity></TouchableOpacity>
+          <TouchableOpacity>
+            <Entypo
+              name="shopping-bag"
+              style={{
+                fontSize: 18,
+                colors: COLOURS.backgroundMedium,
+                padding: 12,
+                borderRadius: 10,
+                backgroundColor: COLOURS.backgroundLight,
+              }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <MaterialCommunityIcons
+              name="cart"
+              style={{
+                fontSize: 18,
+                colors: COLOURS.backgroundMedium,
+                padding: 12,
+                borderRadius: 10,
+                borderWidth: 1,
+                borderColor: COLOURS.backgroundLight,
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            marginBottom: 10,
+            padding: 16,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 26,
+              color: COLOURS.black,
+              fontWeight: "500",
+              letterSpacing: 1,
+              marginBottom: 10,
+            }}
+          >
+            Hi-Fi Shop &amp; Service
+          </Text>
+          <Text
+            style={{
+              fontSize: 14,
+              color: COLOURS.black,
+              fontWeight: "400",
+              letterSpacing: 1,
+              lineHeight: 24,
+            }}
+          >
+            Audio shop on Rustaveli Ave 57.
+            {"\n"}This shop offers both products and services
+          </Text>
+        </View>
+        <View
+          style={{
+            padding: 16,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text
+              style={{
+                fontSize: 18,
+                color: COLOURS.black,
+                fontWeight: "500",
+                letterSpacing: 1,
+              }}
+            >
+              Products
+            </Text>
+            <Text
+              style={{
+                fontSize: 14,
+                color: COLOURS.black,
+                fontWeight: "400",
+                letterSpacing: 0.5,
+                marginLeft: 10,
+              }}
+            >
+              41
+            </Text>
+          </View>
+          <Text
+            style={{
+              fontSize: 14,
+              color: COLOURS.blue,
+              fontWeight: "400",
+            }}
+          >
+            See All
+          </Text>
+        </View>
+        <View>
+          {products.map((data) => {
+            return <ProductCard data={data} id={data.id} />;
+          })}
         </View>
       </ScrollView>
     </View>
